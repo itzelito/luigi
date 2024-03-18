56dc917091f9f42e4ce4d5c923a988914e5765ed
ItzelMaldonado <itzel.mn@gmail.com>
2024-03-18 15:53:26 +0100

    Se puso un @RequestMapping en toda la clasecontrollador de Pizzas para que en cada GET POST DELETE etc no tengamos que poner pizzas/ bla bla en la url

--- src/main/java/be/vdab/luigi/pizzas/PizzaController.java
+++ src/main/java/be/vdab/luigi/pizzas/PizzaController.java
@@ -10,12 +10,13 @@ import java.time.LocalDateTime;
 import java.util.stream.Stream;
 
 @RestController
+@RequestMapping("pizzas")
 class PizzaController {
     private final PizzaService pizzaService;
     PizzaController(PizzaService pizzaService) {
         this.pizzaService = pizzaService;
     }
-    @GetMapping("pizzas/aantal")
+    @GetMapping("aantal")
     long findAantal(){
         return pizzaService.findAantal();
     }
@@ -26,40 +27,40 @@ class PizzaController {
             this(pizza.getId(), pizza.getNaam(), pizza.getPrijs());
         }
     }
-    @GetMapping("pizzas/{id}")
+    @GetMapping("{id}")
     IdNaamPrijs findById(@PathVariable long id){
         return pizzaService.findById(id)
                 .map(pizza -> new IdNaamPrijs(pizza))
                 .orElseThrow(()-> new PizzaNietGevondenException(id));
     }
-    @GetMapping("pizzas")
+    @GetMapping
     Stream<IdNaamPrijs> findAll(){
         return pizzaService.findAll()
                 .stream()
                 .map(pizza -> new IdNaamPrijs(pizza));
     }
-    @GetMapping(value = "pizzas", params = "naamBevat")
+    @GetMapping(params = "naamBevat")
     Stream<IdNaamPrijs> findByNaamBevat (String naamBevat) {
         return pizzaService.findByNaamBevat(naamBevat)
                 .stream()
                 .map(pizza -> new IdNaamPrijs(pizza));
     }
-    @GetMapping(value= "pizzas", params = {"vanPrijs", "totPrijs"})
+    @GetMapping(params = {"vanPrijs", "totPrijs"})
     Stream<IdNaamPrijs> findByPrijsTussen (BigDecimal vanPrijs, BigDecimal totPrijs){
         return pizzaService.findByPrijsTussen(vanPrijs, totPrijs)
                 .stream()
                 .map(pizza -> new IdNaamPrijs(pizza));
     }
-    @DeleteMapping("pizzas/{id}")
+    @DeleteMapping("{id}")
     void delete(@PathVariable long id){
         pizzaService.delete(id);
     }
-    @PostMapping("pizzas")
+    @PostMapping
     long create(@RequestBody @Valid NieuwePizza nieuwePizza){
         var id = pizzaService.create(nieuwePizza);
         return id;
     }
-    @PatchMapping("pizzas/{id}/prijs")
+    @PatchMapping("{id}/prijs")
     void updatePris(@PathVariable long id, @RequestBody @NotNull @PositiveOrZero BigDecimal nieuwePrijs){
         var prijs = new Prijs(nieuwePrijs, LocalDateTime.now(), id);
         pizzaService.updatePrijs(prijs);
@@ -70,7 +71,7 @@ class PizzaController {
             this(prijs.getPrijs(), prijs.getVanaf());
         }
     }
-    @GetMapping("pizzas/{id}/prijzen")
+    @GetMapping("{id}/prijzen")
     Stream<PrijsVanaf> findPrijzen(@PathVariable long id){
         return pizzaService.findPrijzen(id)
                 .stream()
